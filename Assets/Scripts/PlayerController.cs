d48dbe2448f281425df23b12295b7191e4d35e8a
schimmelc <schimmelc4422@my.uwstout.edu>
2015-02-15 18:45:00 -0600

    added First level
    first level with some ui stuff(switching between character states). Also
    started the UI state machine.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -17,12 +18,42 @@ public class PlayerController : MonoBehaviour {
 		public override void OnEnter (PlayerController owner)
 		{
 			owner.stateMachine.setState("Muskalo");
+            Color m = owner.muskaloUI.GetComponent<Image>().color;
+            m.a = 1.0f;
+            owner.muskaloUI.GetComponent<Image>().color = m;
 		}
 		
 		public override void Process (PlayerController owner)
 		{
+            if (owner.foxPowerLevelUI.GetComponent<Slider>().value < owner.foxSpiritStartPowerLevel) // check if fox power level needs to decrease
+            {
+                owner.foxPowerLevelUI.GetComponent<Slider>().value += Time.deltaTime * 0.5f; //increase fox power level
+                owner.foxSpiritPowerLevel = owner.foxPowerLevelUI.GetComponent<Slider>().value;
+            }
+            else if (owner.foxPowerLevelUI.GetComponent<Slider>().value > owner.foxSpiritStartPowerLevel)
+            {
+                owner.foxPowerLevelUI.GetComponent<Slider>().value = owner.foxSpiritStartPowerLevel;
+                owner.foxSpiritPowerLevel = owner.foxPowerLevelUI.GetComponent<Slider>().value;
+            }
 
+            if (owner.wolfPowerLevelUI.GetComponent<Slider>().value < owner.wolfSpiritStartPowerLevel) // check if wolf power level needs to decrease
+            {
+                owner.wolfPowerLevelUI.GetComponent<Slider>().value += Time.deltaTime * 0.5f; //increase wolf power level
+                owner.wolfSpiritPowerLevel = owner.wolfPowerLevelUI.GetComponent<Slider>().value;
+            }
+            else if (owner.wolfPowerLevelUI.GetComponent<Slider>().value > owner.wolfSpiritStartPowerLevel)
+            {
+                owner.wolfPowerLevelUI.GetComponent<Slider>().value = owner.wolfSpiritStartPowerLevel;
+                owner.wolfSpiritPowerLevel = owner.wolfPowerLevelUI.GetComponent<Slider>().value;
+            }
 		}
+
+        public override void OnExit(PlayerController owner)
+        {
+            Color m = owner.muskaloUI.GetComponent<Image>().color;
+            m.a = 0.15f;
+            owner.muskaloUI.GetComponent<Image>().color = m;
+        }
 		
 		void moveLeft(PlayerController owner, params object[] args)
 		{
@@ -44,6 +75,68 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+    // Sun Spirit is fox
+    public class Fox : State<PlayerController>
+    {
+
+        public Fox()
+        {
+            HandlerList.Add(new Handler<PlayerController>("Left", moveLeft));
+            HandlerList.Add(new Handler<PlayerController>("Stop", stopMoving));
+            HandlerList.Add(new Handler<PlayerController>("Right", moveRight));
+            HandlerList.Add(new Handler<PlayerController>("Jump", jump));
+        }
+
+        public override void OnEnter(PlayerController owner)
+        {
+            owner.stateMachine.setState("Fox");
+            Color m = owner.foxUI.GetComponent<Image>().color;
+            m.a = 1.0f;
+            owner.foxUI.GetComponent<Image>().color = m;
+        }
+
+        public override void Process(PlayerController owner)
+        {
+            if (owner.foxPowerLevelUI.GetComponent<Slider>().value > 0) // check if fox power level needs to decrease
+            {
+                owner.foxPowerLevelUI.GetComponent<Slider>().value -= Time.deltaTime; //decrease fox power level
+                owner.foxSpiritPowerLevel = owner.foxPowerLevelUI.GetComponent<Slider>().value;
+            }
+            else
+            {
+                owner.foxPowerLevelUI.GetComponent<Slider>().value = 0;
+                owner.foxSpiritPowerLevel = owner.foxPowerLevelUI.GetComponent<Slider>().value;
+                owner.stateMachine.ChangeState(owner.states[0]); //switch state to muskalo
+            }
+        }
+
+        public override void OnExit(PlayerController owner)
+        {
+            Color m = owner.foxUI.GetComponent<Image>().color;
+            m.a = 0.15f;
+            owner.foxUI.GetComponent<Image>().color = m;
+        }
+
+        void moveLeft(PlayerController owner, params object[] args)
+        {
+
+        }
+
+        void moveRight(PlayerController owner, params object[] args)
+        {
+
+        }
+
+        void jump(PlayerController owner, params object[] args)
+        {
+
+        }
+        void stopMoving(PlayerController owner, params object[] args)
+        {
+
+        }
+    }
+
 	// Moon Spirit is wolf
 	public class Wolf : State<PlayerController>{
 
@@ -58,12 +151,32 @@ public class PlayerController : MonoBehaviour {
 		public override void OnEnter (PlayerController owner)
 		{
 			owner.stateMachine.setState("Wolf");
+            Color m = owner.wolfUI.GetComponent<Image>().color;
+            m.a = 1.0f;
+            owner.wolfUI.GetComponent<Image>().color = m;
 		}
 		
 		public override void Process (PlayerController owner)
 		{
-
+            if (owner.wolfPowerLevelUI.GetComponent<Slider>().value > 0) // check if wolf power level needs to decrease
+            {
+                owner.wolfPowerLevelUI.GetComponent<Slider>().value -= Time.deltaTime; //decrease wolf power level
+                owner.wolfSpiritPowerLevel = owner.wolfPowerLevelUI.GetComponent<Slider>().value;
+            }
+            else
+            {
+                owner.wolfPowerLevelUI.GetComponent<Slider>().value = 0;
+                owner.wolfSpiritPowerLevel = owner.wolfPowerLevelUI.GetComponent<Slider>().value;
+                owner.stateMachine.ChangeState(owner.states[0]); //switch state to muskalo
+            }
 		}
+
+        public override void OnExit(PlayerController owner)
+        {
+            Color m = owner.wolfUI.GetComponent<Image>().color;
+            m.a = 0.15f;
+            owner.wolfUI.GetComponent<Image>().color = m;
+        }
 		
 		void moveLeft(PlayerController owner, params object[] args)
 		{
@@ -86,24 +199,54 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	public State<PlayerController>[] states = new State<PlayerController>[] {	new Muskalo(),
-																				new Wolf()};
+																				new Wolf(),
+                                                                                new Fox()};
 	public StateMachine<PlayerController> stateMachine = new StateMachine<PlayerController>();
+
+    public GameObject muskaloUI;
+    public GameObject wolfUI;
+    public GameObject foxUI;
+    public GameObject foxPowerLevelUI;
+    public GameObject wolfPowerLevelUI;
+    public float wolfSpiritPowerLevel = 20;
+    public float foxSpiritPowerLevel = 20;
+
+    float foxSpiritStartPowerLevel;
+    float wolfSpiritStartPowerLevel;
 
 	// Use this for initialization
 	void Start () {
-	
+        foxSpiritStartPowerLevel = foxSpiritPowerLevel;
+        wolfSpiritStartPowerLevel = wolfSpiritPowerLevel;
+        stateMachine.Configure(this, states[0]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        stateMachine.Update();
+        if (Input.GetKeyUp(KeyCode.Q))
+            Left();
+        else if (Input.GetKeyUp(KeyCode.E))
+            Right();
 	}
 
 	void Left(){
 		stateMachine.messageReciever("Left",null);
+        if(stateMachine.getState() == "Wolf")
+            stateMachine.ChangeState(states[0]);
+        else if (stateMachine.getState() == "Fox")
+            stateMachine.ChangeState(states[1]);
+        else if (stateMachine.getState() == "Muskalo")
+            stateMachine.ChangeState(states[2]);
 	}
 	
 	void Right(){
 		stateMachine.messageReciever("Right",null);
+        if (stateMachine.getState() == "Wolf")
+            stateMachine.ChangeState(states[2]);
+        else if (stateMachine.getState() == "Fox")
+            stateMachine.ChangeState(states[0]);
+        else if (stateMachine.getState() == "Muskalo")
+            stateMachine.ChangeState(states[1]);
 	}
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,112 +2,118 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour
+{
 
-    public GameObject moonPowerBar;
-    public GameObject moonPowerImage;
-    public GameObject moonSprite;
-
-    public GameObject sunPowerBar;
-    public GameObject sunPowerImage;
-    public GameObject sunSprite;
-
-    public GameObject muskaloSprite;
-
-    public float moonSpiritPowerLevel = 20;
-    public float sunSpiritPowerLevel = 20;
-
-    float sunSpiritStartPowerLevel;
-    float moonSpiritStartPowerLevel;
-
-    public enum Characters { Moon, Sun, Muskalo }
-
-    Characters CurrentCharcter;
-
-    //GameObject character;
-
-	// Use this for initialization
-	void Start () {
-        //character = GameObject.Find("ThirdPersonCharacter");
-        sunSpiritStartPowerLevel = sunSpiritPowerLevel;
-        moonSpiritStartPowerLevel = moonSpiritPowerLevel;
-        CurrentCharcter = Characters.Muskalo;
-        moonSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-        sunSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    public class RotatingLeft : State<UIController>
     {
-        sunPowerImage.GetComponent<Image>().fillAmount = sunSpiritPowerLevel / sunSpiritStartPowerLevel;
-        moonPowerImage.GetComponent<Image>().fillAmount = moonSpiritPowerLevel / moonSpiritStartPowerLevel;
+        public RotatingLeft()
+        {
+            
+        }
 
-        sunPowerBar.GetComponent<Slider>().value = sunSpiritPowerLevel;
-        moonPowerBar.GetComponent<Slider>().value = moonSpiritPowerLevel;
+        public override void OnEnter(UIController owner)
+        {
+            
+        }
 
-        if (CurrentCharcter == Characters.Muskalo)
+        public override void Process(UIController owner)
         {
-            //character.GetComponent<ThirdPersonCharacter>().jumpPower = 10;
-            //character.GetComponent<ThirdPersonUserControl>().walkByDefault = true;
-            if (Input.GetKeyUp(KeyCode.E))
-            {
-                CurrentCharcter = Characters.Moon;
-                muskaloSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-                moonSprite.GetComponent<Image>().color += new Color(0, 0, 0, 0.85f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Q))
-            {
-                CurrentCharcter = Characters.Sun;
-                muskaloSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-                sunSprite.GetComponent<Image>().color += new Color(0, 0, 0, 0.85f);
-            }
-            if (moonSpiritPowerLevel < moonSpiritStartPowerLevel)
-                moonSpiritPowerLevel += Time.deltaTime/2;
-            else if (moonSpiritPowerLevel > moonSpiritStartPowerLevel)
-                moonSpiritPowerLevel = moonSpiritStartPowerLevel;
-            if(sunSpiritPowerLevel < sunSpiritStartPowerLevel)
-                sunSpiritPowerLevel += Time.deltaTime/2;
-            else if (sunSpiritPowerLevel > sunSpiritStartPowerLevel)
-                sunSpiritPowerLevel = sunSpiritStartPowerLevel;
+            
         }
-        else if (CurrentCharcter == Characters.Moon)
+
+        public override void OnExit(UIController owner)
         {
-            //character.GetComponent<ThirdPersonCharacter>().jumpPower = 20;
-            //character.GetComponent<ThirdPersonUserControl>().walkByDefault = true;
-            if (moonSpiritPowerLevel > 0)
-                moonSpiritPowerLevel -= Time.deltaTime;
-            if (Input.GetKeyUp(KeyCode.E))
-            {
-                CurrentCharcter = Characters.Sun;
-                moonSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-                sunSprite.GetComponent<Image>().color += new Color(0, 0, 0, 0.85f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Q) || moonSpiritPowerLevel <= 0)
-            {
-                CurrentCharcter = Characters.Muskalo;
-                muskaloSprite.GetComponent<Image>().color += new Color(0, 0, 0, 0.85f);
-                moonSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-            }
+            
         }
-        else if (CurrentCharcter == Characters.Sun)
+    }
+
+
+    public class RotatingRight : State<UIController>
+    {
+
+        public RotatingRight()
         {
-            //character.GetComponent<ThirdPersonCharacter>().jumpPower = 10;
-            //character.GetComponent<ThirdPersonUserControl>().walkByDefault = false;
-            if (sunSpiritPowerLevel > 0)
-                sunSpiritPowerLevel -= Time.deltaTime;
-            if (Input.GetKeyUp(KeyCode.E) || sunSpiritPowerLevel <= 0)
-            {
-                CurrentCharcter = Characters.Muskalo;
-                muskaloSprite.GetComponent<Image>().color += new Color(0, 0, 0, 0.85f);
-                sunSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-            }
-            else if (Input.GetKeyUp(KeyCode.Q))
-            {
-                CurrentCharcter = Characters.Moon;
-                moonSprite.GetComponent<Image>().color += new Color(0, 0, 0, 0.85f);
-                sunSprite.GetComponent<Image>().color += new Color(0, 0, 0, -0.85f);
-            }
+            
         }
+
+        public override void OnEnter(UIController owner)
+        {
+            
+        }
+
+        public override void Process(UIController owner)
+        {
+            
+        }
+
+        public override void OnExit(UIController owner)
+        {
+            
+        }
+    }
+
+    public class Stationary : State<UIController>
+    {
+
+        public Stationary()
+        {
+            
+        }
+
+        public override void OnEnter(UIController owner)
+        {
+            
+        }
+
+        public override void Process(UIController owner)
+        {
+            
+        }
+
+        public override void OnExit(UIController owner)
+        {
+            
+        }
+    }
+
+    public State<UIController>[] states = new State<UIController>[] {	new Stationary(),
+																				new RotatingLeft(),
+                                                                                new RotatingRight()};
+    public StateMachine<UIController> stateMachine = new StateMachine<UIController>();
+
+    public GameObject muskaloUI;
+    public GameObject wolfUI;
+    public GameObject foxUI;
+    public GameObject foxPowerLevelUI;
+    public GameObject wolfPowerLevelUI;
+    public float wolfSpiritPowerLevel = 20;
+    public float foxSpiritPowerLevel = 20;
+
+    float foxSpiritStartPowerLevel;
+    float wolfSpiritStartPowerLevel;
+
+    // Use this for initialization
+    void Start()
+    {
+        foxSpiritStartPowerLevel = foxSpiritPowerLevel;
+        wolfSpiritStartPowerLevel = wolfSpiritPowerLevel;
+        stateMachine.Configure(this, states[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.Update();
+    }
+
+    void Left()
+    {
+
+    }
+
+    void Right()
+    {
+
     }
 }
