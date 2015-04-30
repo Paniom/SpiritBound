c5e06fe93b65f28a0eb5b7acac594cca8d5bcd01
bootse <bootalmighty@gmail.com>
2015-04-30 00:07:13 -0500

    Adding Mobile Controls
    First Pass

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -18,31 +18,13 @@ public class InputController : MonoBehaviour {
 	private float interactDelay = 1.5f;
 	private float interactTime = 0;
 
-	private float maxSpeed = 1.4f;
-	private float speedChange = .35f;
+	public float maxSpeed = 1.4f;
+	public float speedChange = .35f;
 	public float ySpeed = 0;
-	private float stopTime = 1.3f;
+	public float stopTime = 1.3f;
 	private float stopTimer = 0;
 	private bool yPress = false;
 	private bool backPress = false;
-
-	private int joystickID = -1; // used to keep track of which finger is controlling the joystick
-	private int slideID = -1; // used to keep track of which finger is doing the swipe
-	private Vector2 joystickPosition; // used for a moveable joystick
-	private Vector2 slideStartPosition; // used for mapping a swipe
-	private float endTime = 0; // used to force an end time on a swipe 
-	private float jumpTime = 0; // used to compare short swipes to taps
-	private float slideTime = 0; // used to show how long a press is going
-	private Vector2 slideDirection; // used to calculate the swipes
-
-	// used for showing a joystick
-	public GameObject cam;
-	public GameObject joystick;
-	public GameObject joystickPrefab;
-	
-	private bool start; // used to set time the start of a swipe
-	private float oWidth = 0.0f; // used to divide the screen to figure out 
-	private float oHeight = 0.0f; // used to find the width
 
 	/*
 	 * Messages used: 
@@ -51,8 +33,6 @@ public class InputController : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		oHeight = 2.0f*Camera.main.orthographicSize;
-		oWidth = oHeight*Camera.main.aspect;
 		// used to find the input type used to play the game
 		#if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_WEBPLAYER
 		inputDevice = InputType.Keyboard;
@@ -130,149 +110,25 @@ public class InputController : MonoBehaviour {
 				backPress = false;
 			}
 			direction.y = ySpeed;
-			/*if (direction.y == 0) {
-				if ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) ) {
-					direction.y = 1;
-				}
-				else if ( Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ) {
-					direction.y = -1;
-				}
-			}
-			if (direction.y > 0) {
-				if ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) ) {
-					direction.y = 1;
-				}
-				else if ( Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ) {
-					direction.y = -1;
-				}
-				else {
-					direction.y = 0;
-				}
-			}
-			if (direction.y < 0) {
-				if ( Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) ) {
-					direction.y = -1;
-				}
-				else if ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) ) {
-					direction.y = 1;
-				}
-				else {
-					direction.y = 0;
-				}
-			}*/
 			direction = direction*6;
 			target.SendMessage("Move", direction, SendMessageOptions.DontRequireReceiver);
-			if ( Input.GetKey(KeyCode.Space) ) {
+			if ( Input.GetKeyDown(KeyCode.Space) ) {
 				target.SendMessage("Jump", SendMessageOptions.DontRequireReceiver);
 			}
-			if(!interactActive && interactTime <= 0) {
-				if ( Input.GetKey(KeyCode.E) ) {
-					target.SendMessage("Interact", SendMessageOptions.DontRequireReceiver);
-					interactActive = true;
-				}
-			}
 			if (canRotate) {
-				if ( Input.GetKey(KeyCode.Alpha1) ) {
+				if ( Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Keypad1) ) {
 					target.SendMessage("ChangeToFox", SendMessageOptions.DontRequireReceiver);
 					//canRotate = false;
 				}
-				else if ( Input.GetKey(KeyCode.Alpha2) ) {
+				else if ( Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Keypad2) ) {
 					target.SendMessage("ChangeToMuskalo", SendMessageOptions.DontRequireReceiver);
 					//canRotate = false;
 				}
-                else if (Input.GetKey(KeyCode.Alpha3))
+				else if (Input.GetKey(KeyCode.Alpha3) || Input.GetKey(KeyCode.Keypad3) )
                 {
                     target.SendMessage("ChangeToWolf", SendMessageOptions.DontRequireReceiver);
                     //canRotate = false;
                 }
-			}
-		}
-		if(inputDevice == InputType.Mobile) {
-			for(int i = 0; i < Input.touchCount; i++){
-				if(Input.touches[i].fingerId == slideID ){
-					if(Input.touches[i].phase == TouchPhase.Moved || Input.touches[i].phase == TouchPhase.Stationary){
-						slideDirection = Input.touches[i].position - slideStartPosition;
-						if(start){
-							endTime = Time.time + 1;
-							jumpTime = Time.time + .2f;
-							start = false;
-						}
-						slideTime = Time.time;
-					}
-					if(Input.touches[i].phase == TouchPhase.Ended || Input.touches[i].phase == TouchPhase.Canceled){
-						slideID = -1;
-						if(slideDirection.sqrMagnitude > 5000){
-							setSwipe(slideDirection);
-						}
-						else {
-							if(start || slideTime < jumpTime) {
-								target.SendMessage("Jump", SendMessageOptions.DontRequireReceiver);
-							}
-						}
-					}
-					if(slideTime < endTime && slideDirection.sqrMagnitude > 40000){
-						slideID = -1;
-						setSwipe(slideDirection);
-					}
-					if(slideTime >= endTime){
-						slideID = -1;
-						if(slideDirection.sqrMagnitude > 5000){
-							setSwipe(slideDirection);
-						}
-					}
-				}
-				else if(slideID == -1 && Input.touches[i].fingerId != joystickID){
-					if(Input.touches[i].position.x > Screen.width/2){
-						if(Input.touches[i].phase == TouchPhase.Began || Input.touches[i].phase == TouchPhase.Moved || Input.touches[i].phase == TouchPhase.Stationary){
-							slideID = Input.touches[i].fingerId;
-							slideStartPosition = Input.touches[i].position;
-							start = true;
-						}
-					}
-				}
-				if(Input.touches[i].fingerId == joystickID){
-					if(Input.touches[i].phase == TouchPhase.Moved || Input.touches[i].phase == TouchPhase.Stationary){
-						direction = Input.touches[i].position - joystickPosition;
-						joystick.GetComponent<Joystick>().position(direction);
-						if(direction.sqrMagnitude > 700){
-							if(direction.sqrMagnitude > 22500){
-								direction = direction.normalized;
-							}
-							else{
-								float rat = direction.magnitude/150f;
-								direction = direction.normalized * rat;
-							}
-							direction = direction*7;
-						}
-						else{
-							direction = Vector2.zero;
-						}
-					}
-					if(Input.touches[i].phase == TouchPhase.Ended || Input.touches[i].phase == TouchPhase.Canceled){
-						joystickID = -1;
-						Destroy(joystick);
-					}
-				}
-				else if(joystickID == -1){
-					if(Input.touches[i].phase == TouchPhase.Began){
-						if(Input.touches[i].position.x < Screen.width/2){
-							joystickID = Input.touches[i].fingerId;
-							joystickPosition = Input.touches[i].position;
-							if(joystick != null){
-								Destroy(joystick);
-							}
-							oHeight = 2.0f*Camera.main.orthographicSize;
-							oWidth = oHeight*Camera.main.aspect;
-							joystick = Instantiate(joystickPrefab) as GameObject;
-							RectTransform rect = joystick.GetComponent<RectTransform>();
-							joystick.transform.parent = cam.transform;
-							rect.localPosition = new Vector3((joystickPosition.x/(Screen.width)*oWidth)-oWidth/2.0f, -(oHeight/2.0f)+(joystickPosition.y/Screen.height)*oHeight, 0);
-							rect.localRotation = Quaternion.Euler(new Vector3(0,0,0));
-							rect.localScale = new Vector3(oWidth/10f,oWidth/10f,1);
-						}
-					}
-				}
-				target.SendMessage("Move", direction, SendMessageOptions.DontRequireReceiver);
 			}
 		}
 	}
@@ -286,52 +142,5 @@ public class InputController : MonoBehaviour {
 
 	void rotateFinished() {
 		canRotate = true;
-	}
-
-	// Determines whether a swipe is vertical up the screen or horizontal and which direction
-	// Sends the appropriate message to the target
-	void setSwipe(Vector2 input) {
-		float x = input.x;
-		float y = input.y;
-
-		if (canRotate && !interactActive) {
-			if(Mathf.Abs(x) > Mathf.Abs(y) && Mathf.Abs(x) > 100) {
-				if(x > 0) {
-					GameObject deathtext = Instantiate(Resources.Load("DeathText")) as GameObject;
-					deathtext.transform.parent = GameObject.Find("UI Canvas").transform;
-					deathtext.GetComponentInChildren<Text>().text = "RotateRight";
-					deathtext.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-					deathtext.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
-					deathtext.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-					Destroy(deathtext, 1.0f);
-					target.SendMessage("RotateRight", SendMessageOptions.DontRequireReceiver);
-					canRotate = false;
-				}
-				else {
-					GameObject deathtext = Instantiate(Resources.Load("DeathText")) as GameObject;
-					deathtext.transform.parent = GameObject.Find("UI Canvas").transform;
-					deathtext.GetComponentInChildren<Text>().text = "RotateLeft";
-					deathtext.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-					deathtext.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
-					deathtext.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-					Destroy(deathtext, 1.0f);
-					target.SendMessage("RotateLeft", SendMessageOptions.DontRequireReceiver);
-					canRotate = false;
-				}
-			}
-		}
-		else if(y > 100) {
-			if(!interactActive && interactTime <= 0) {
-				GameObject deathtext = Instantiate(Resources.Load("DeathText")) as GameObject;
-				deathtext.transform.parent = GameObject.Find("UI Canvas").transform;
-				deathtext.GetComponentInChildren<Text>().text = "Interact";
-				deathtext.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-				deathtext.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
-				deathtext.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-				Destroy(deathtext, 1.0f);
-				target.SendMessage("Interact", SendMessageOptions.DontRequireReceiver);
-				interactActive = true;
-			}
-		}
 	}
 }
---
+++ Assets/Scripts/MobileControls.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileControls : MonoBehaviour {
+
+	bool left = false;
+	bool right = false;
+	bool down = false;
+	bool up = false;
+	bool yPress = false;
+	public InputController playerInput;
+	float downTimer = 1.3f;
+	float upTimer = 1f;
+	float ySpeed = 0;
+	float stopTimer = 0;
+	float stopTime = 1.4f;
+	float speedChange = .35f;
+	float maxSpeed = 1.4f;
+	bool backPress = false;
+
+	Vector2 direction = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8 || UNITY_BLACKBERRY
+		gameObject.SetActive(false);
+		#endif
+		downTimer = playerInput.stopTime;
+		maxSpeed = playerInput.maxSpeed;
+		speedChange = playerInput.speedChange;
+		stopTime = playerInput.stopTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction.x == 0) {
+			if ( left ) {
+				direction.x = -1;
+			}
+			else if ( right ) {
+				direction.x = 1;
+			}
+		}
+		if (direction.x > 0) {
+			if ( right ) {
+				direction.x = 1;
+			}
+			else if ( left ) {
+				direction.x = -1;
+			}
+			else {
+				direction.x = 0;
+			}
+		}
+		if (direction.x < 0) {
+			if ( left ) {
+				direction.x = -1;
+			}
+			else if ( right ) {
+				direction.x = 1;
+			}
+			else {
+				direction.x = 0;
+			}
+		}
+		if(down)
+		{
+			if(stopTimer > stopTime)
+			{
+				ySpeed = 0;
+			}
+			stopTimer += Time.deltaTime;
+		}
+		direction.y = ySpeed;
+		direction = direction*6;
+		playerInput.target.SendMessage("Move", direction, SendMessageOptions.DontRequireReceiver);
+	}
+
+	public void LeftPressed() {
+		left = true;
+	}
+
+	public void LeftReleased() {
+		left = false;
+	}
+
+	public void RightPressed() {
+		right = true;
+	}
+
+	public void RightReleased() {
+		right = false;
+	}
+
+	public void Jump() {
+		playerInput.target.SendMessage("Jump", SendMessageOptions.DontRequireReceiver);
+	}
+
+	public void UpPressed() {
+		up = true;
+		ySpeed = Mathf.Clamp(ySpeed + speedChange, 0, maxSpeed);
+	}
+
+	public void UpReleased() {
+		up = false;
+	}
+
+	public void DownPressed() {
+		down = true;
+		ySpeed = Mathf.Clamp(ySpeed - speedChange, 0, maxSpeed);
+		stopTimer = 0;
+	}
+
+	public void DownReleased() {
+		down = false;
+	}
+}
